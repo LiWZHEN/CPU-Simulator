e8b3199d314e3971ffaccb4d46b19fe8848ceab0
livandan <livandan@sjtu.edu.cn>
2025-07-23 00:31:31 +0800

    Remove InstructionMemory.

--- singleStagePipeline/src/InstructionMemory.cpp
+++
@@ -1,9 +0,0 @@
-#include "../include/InstructionMemory.hpp"
-
-void InstructionMemory::AddInstruction(const int32_t address, const std::string &instruction) {
-  instructions[address] = instruction;
-}
-
-std::string InstructionMemory::GetInstruction(const int32_t address) const {
-  return instructions.at(address);
-}
