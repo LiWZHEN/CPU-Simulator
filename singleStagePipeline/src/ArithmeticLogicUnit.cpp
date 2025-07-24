22f8f71d5d4e2348cfcc10413d59757ac79b6b5a
livandan <livandan@sjtu.edu.cn>
2025-07-25 01:17:02 +0800

    Simplify the logic of srl (just turn the number to be shifted into unsigned int, then it can move rightwards freely).

--- singleStagePipeline/src/ArithmeticLogicUnit.cpp
+++ singleStagePipeline/src/ArithmeticLogicUnit.cpp
@@ -29,13 +29,8 @@ int32_t ArithmeticLogicUnit::Sll(const int32_t n1, const int32_t n2) {
 }
 
 int32_t ArithmeticLogicUnit::Srl(const int32_t n1, const int32_t n2) {
-  if (n1 & 0b10000000000000000000000000000000 == 0) {
-    return n1 >> n2;
-  }
-  if (n2 == 0) {
-    return n1;
-  }
-  return ((n1 >> 1) | 0b01111111111111111111111111111111) >> (n2 - 1);
+  const uint32_t un1 = n1;
+  return un1 >> n2;
 }
 
 int32_t ArithmeticLogicUnit::Sra(const int32_t n1, const int32_t n2) {
