ae31c7bf329947ffa74053d33b0e8bbb4c4431ac
livandan <livandan@sjtu.edu.cn>
2025-07-25 01:21:23 +0800

    Remove copy constructor.

--- singleStagePipeline/src/RegFile.cpp
+++ singleStagePipeline/src/RegFile.cpp
@@ -1,11 +1,5 @@
 #include "../include/RegFile.hpp"
 
-RegFile::RegFile(const RegFile &other) {
-  for (int i = 0; i < 32; ++i) {
-    registers[i] = other.registers[i];
-  }
-}
-
 void RegFile::Modify(const int32_t ind, const int32_t x) {
   registers[ind] = x;
   if (ind == 0) {
