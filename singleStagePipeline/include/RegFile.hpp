a8b6a34ec7b00657b4579b33b8146f3db6085781
livandan <livandan@sjtu.edu.cn>
2025-07-25 01:14:05 +0800

    Initialize the registers with 0 (git add singleStagePipeline/include/RegFile.hppgit add singleStagePipeline/include/RegFile.hpp), and remove useless copy-construct function.

--- singleStagePipeline/include/RegFile.hpp
+++ singleStagePipeline/include/RegFile.hpp
@@ -4,10 +4,9 @@
 #include <cstdint>
 
 class RegFile {
-  int32_t registers[32];
+  int32_t registers[32] = {};
 public:
   RegFile() = default;
-  RegFile(const RegFile &other);
   void Modify(const int32_t ind, const int32_t x);
   int32_t GetData(const int32_t ind) const;
 };
