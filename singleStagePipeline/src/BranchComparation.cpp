867c46c79269ddbfed63acea22f3516c29659e18
livandan <livandan@sjtu.edu.cn>
2025-07-25 15:01:13 +0800

    Compare urs1 and urs2 in unsigned comparation.

--- singleStagePipeline/src/BranchComparation.cpp
+++ singleStagePipeline/src/BranchComparation.cpp
@@ -10,7 +10,7 @@ bool BranchComparation::SignedGreaterOrEqual(const int32_t rs1, const int32_t rs
 
 bool BranchComparation::UnsignedGreaterOrEqual(const int32_t rs1, const int32_t rs2) {
   const uint32_t urs1 = rs1, urs2 = rs2;
-  return rs1 >= rs2;
+  return urs1 >= urs2;
 }
 
 bool BranchComparation::SignedLess(const int32_t rs1, const int32_t rs2) {
@@ -19,7 +19,7 @@ bool BranchComparation::SignedLess(const int32_t rs1, const int32_t rs2) {
 
 bool BranchComparation::UnsignedLess(const int32_t rs1, const int32_t rs2) {
   const uint32_t urs1 = rs1, urs2 = rs2;
-  return rs1 < rs2;
+  return urs1 < urs2;
 }
 
 bool BranchComparation::NotEqual(const int32_t rs1, const int32_t rs2) {
