f2c866d76a875a21c716f510e24fb94d7b5a3a87
livandan <livandan@sjtu.edu.cn>
2025-07-25 11:11:11 +0800

    Simplify the logic of unsigned comparation.

--- singleStagePipeline/src/BranchComparation.cpp
+++ singleStagePipeline/src/BranchComparation.cpp
@@ -9,12 +9,8 @@ bool BranchComparation::SignedGreaterOrEqual(const int32_t rs1, const int32_t rs
 }
 
 bool BranchComparation::UnsignedGreaterOrEqual(const int32_t rs1, const int32_t rs2) {
-  for (int i = 31; i >= 0; --i) {
-    if ((rs1 & (1 << i)) == 0 && (rs1 & (1 << i)) != 0) {
-      return false;
-    }
-  }
-  return true;
+  const uint32_t urs1 = rs1, urs2 = rs2;
+  return rs1 >= rs2;
 }
 
 bool BranchComparation::SignedLess(const int32_t rs1, const int32_t rs2) {
@@ -22,7 +18,8 @@ bool BranchComparation::SignedLess(const int32_t rs1, const int32_t rs2) {
 }
 
 bool BranchComparation::UnsignedLess(const int32_t rs1, const int32_t rs2) {
-  return !UnsignedGreaterOrEqual(rs1, rs2);
+  const uint32_t urs1 = rs1, urs2 = rs2;
+  return rs1 < rs2;
 }
 
 bool BranchComparation::NotEqual(const int32_t rs1, const int32_t rs2) {
