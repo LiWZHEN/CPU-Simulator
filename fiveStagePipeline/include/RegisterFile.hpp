5e274d86f7bb97363eaf9da78c6abdc01ce6ccb3
livandan <livandan@sjtu.edu.cn>
2025-07-31 01:50:59 +0800

    Complete RegisterFile.

--- fiveStagePipeline/include/RegisterFile.hpp
+++ fiveStagePipeline/include/RegisterFile.hpp
@@ -2,19 +2,42 @@
 #define REGISTER_FILE_HPP
 
 #include <cstdint>
+#include "Decoder.hpp"
+
+struct TaskUnitFromROB {
+  InstructionType type = InstructionType::NONE;
+  int32_t rd = -1, value = 0, rob_ind = -1;
+};
+
+struct RFTask {
+  bool predict_failed = false;
+  int32_t ind_from_decoder = 0;
+  int32_t dependence_from_decoder = -1;
+  TaskUnitFromROB task_from_rob[32];
+  int32_t task_from_rob_len = 0;
+};
 
 class RegisterFile {
-  int32_t old_reg[32] = {};
-  int32_t new_reg[32] = {};
-  int32_t old_dependence[32] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-      -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
-  int32_t new_dependence[32] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+  Decoder *decoder = nullptr;
+  LSB *lsb = nullptr;
+
+  RFTask task;
+  
+  bool predict_failed = false;
+  int32_t ind_from_decoder = 0;
+  int32_t dependence_from_decoder = -1;
+  TaskUnitFromROB task_from_rob[32];
+  int32_t task_from_rob_len = 0;
+  int32_t rf[32] = {0};
+  int32_t dependence[32] = {-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
       -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1};
 public:
   RegisterFile() = default;
-  void Modify(const int32_t ind, const int32_t x);
-  int32_t GetData(const int32_t ind) const;
-  int32_t GetDependence(const int32_t ind) const;
+  void Connect(Decoder *decoder, LSB *lsb);
+  void SetNewDepenence(int32_t rd, int32_t dependence);
+  void SetFromROB(InstructionType type, int32_t rd, int32_t value, int32_t rob_ind);
+  void PredictFailed();
   void Update();
+  void Run();
 };
 #endif
--- fiveStagePipeline/src/RegisterFile.cpp
+++ fiveStagePipeline/src/RegisterFile.cpp
@@ -1,23 +1,55 @@
 #include "../include/RegisterFile.hpp"
 
-void RegisterFile::Modify(const int32_t ind, const int32_t x) {
-  new_reg[ind] = x;
-  if (ind == 0) {
-    new_reg[0] = 0;
-  }
-}
-
-int32_t RegisterFile::GetData(const int32_t ind) const {
-  return old_reg[ind];
-}
-
-int32_t RegisterFile::GetDependence(const int32_t ind) const {
-  return old_dependence[ind];
+void RegisterFile::Connect(Decoder *decoder, LSB *lsb) {
+  this->decoder = decoder;
+  this->lsb = lsb;
 }
 
 void RegisterFile::Update() {
-  for (int i = 0; i < 32; ++i) {
-    old_reg[i] = new_reg[i];
-    old_dependence[i] = new_dependence[i];
+  predict_failed = task.predict_failed;
+  ind_from_decoder = task.ind_from_decoder;
+  dependence_from_decoder = task.dependence_from_decoder;
+  task_from_rob_len = task.task_from_rob_len;
+  for (int i = 0; i < task_from_rob_len; ++i) {
+    task_from_rob[i] = task.task_from_rob[i];
   }
+  task.task_from_rob_len = 0;
+  task.predict_failed = false;
+  task.ind_from_decoder = 0;
+  task.dependence_from_decoder = -1;
+}
+
+void RegisterFile::SetNewDepenence(int32_t rd, int32_t dependence) {
+  task.ind_from_decoder = rd;
+  task.dependence_from_decoder = dependence;
+}
+
+void RegisterFile::PredictFailed() {
+  task.predict_failed = true;
+}
+
+void RegisterFile::Run() {
+  for (int i = 0; i < task_from_rob_len; ++i) {
+    if (task_from_rob[i].type == InstructionType::SB
+        || task_from_rob[i].type == InstructionType::SH
+        || task_from_rob[i].type == InstructionType::SW) {
+      lsb->SetStoreFromRF(task_from_rob[i].value, rf[task_from_rob[i].rd], task_from_rob[i].rob_ind);
+    } else {
+      if (dependence[task_from_rob[i].rd] == task_from_rob[i].rob_ind) {
+        dependence[task_from_rob[i].rd] = -1;
+      }
+      rf[task_from_rob[i].rd] = task_from_rob[i].value;
+    }
+  }
+  dependence[ind_from_decoder] = dependence_from_decoder;
+  if (predict_failed) {
+    for (int i = 0; i < 32; ++i) {
+      dependence[i] = -1;
+    }
+  }
+  decoder->PassRF(rf, dependence);
+}
+
+void RegisterFile::SetFromROB(InstructionType type, int32_t rd, int32_t value, int32_t rob_ind) {
+  task.task_from_rob[task.task_from_rob_len++] = {type, rd, value, rob_ind};
 }
