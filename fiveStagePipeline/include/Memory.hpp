115ae278b122cd9f4ed8f626dd56df3d2acc56a4
livandan <livandan@sjtu.edu.cn>
2025-07-31 02:50:00 +0800

    Complete Memory.

--- fiveStagePipeline/include/Memory.hpp
+++ fiveStagePipeline/include/Memory.hpp
@@ -5,27 +5,40 @@
 #include <queue>
 #include <cstdint>
 #include <string>
-#include "RegisterFile.hpp"
+
+#include "LoadStoreBuffer.hpp"
+#include "Decoder.hpp"
+#include "ReorderBuffer.hpp"
+#include "ReservationStation.hpp"
+
+struct MemoryTaskUnit {
+  InstructionType type = InstructionType::NONE;
+  int32_t address = -1, value = 0;
+};
+
+struct MemoryTask {
+  MemoryTaskUnit memory_tasks[32];
+  int32_t memory_tasks_len = 0;
+};
 
 class Memory {
-  struct ModifyRecordUnit {
-    int32_t index;
-    int32_t value;
-    ModifyRecordUnit() = default;
-    ModifyRecordUnit(const int32_t ind, const int32_t val) {
-      index = ind;
-      value = val;
-    }
-  };
-  std::unordered_map<int32_t, uint8_t> old_memories;
-  std::unordered_map<int32_t, uint8_t> new_memories;
-  std::queue<ModifyRecordUnit> modify_record;
-  RegisterFile *register_file = nullptr;
+  LSB *lsb = nullptr;
+  Decoder *decoder = nullptr;
+  ROB *rob = nullptr;
+  RS *rs = nullptr;
+
+  MemoryTask task;
+
+  MemoryTaskUnit memory_tasks[32];
+  int32_t memory_tasks_len = 0;
+  std::unordered_map<int32_t, uint8_t> memories;
 public:
   Memory() = default;
-  void Connect(RegisterFile *register_file);
+  void Connect(LSB *lsb, Decoder *decoder, ROB *rob, RS *rs);
   void Add(const int32_t address, const uint8_t data);
   int32_t Get(const int32_t address) const;
   void Update();
+  void Run();
+  void SetTask(InstructionType type, int32_t address, int32_t value);
 };
 #endif
--- fiveStagePipeline/src/Memory.cpp
+++ fiveStagePipeline/src/Memory.cpp
@@ -1,25 +1,76 @@
 #include "../include/Memory.hpp"
 
-void Memory::Connect(RegisterFile *register_file) {
-  this->register_file = register_file;
+void Memory::Connect(LSB *lsb, Decoder *decoder, ROB *rob, RS *rs) {
+  this->lsb = lsb;
+  this->decoder = decoder;
+  this->rob = rob;
+  this->rs = rs;
 }
 
 void Memory::Add(const int32_t address, const uint8_t data) {
-  new_memories[address] = data;
-  modify_record.push(ModifyRecordUnit(address, data));
+  memories[address] = data;
 }
 
 int32_t Memory::Get(const int32_t address) const {
-  if (old_memories.count(address) == 0) {
+  if (memories.count(address) == 0) {
     return 0;
   }
-  return old_memories.at(address);
+  return memories.at(address);
 }
 
 void Memory::Update() {
-  while (!modify_record.empty()) {
-    auto top_record = modify_record.front();
-    modify_record.pop();
-    old_memories[top_record.index] = top_record.value;
+  memory_tasks_len = task.memory_tasks_len;
+  for (int i = 0; i < memory_tasks_len; ++i) {
+    memory_tasks[i] = task.memory_tasks[i];
   }
+  task.memory_tasks_len = 0;
+}
+
+void Memory::Run() {
+  for (int i = 0; i < memory_tasks_len; ++i) {
+    MemoryTaskUnit unit_task = memory_tasks[i];
+    if (unit_task.type == InstructionType::LB) {
+      int32_t data_m = this->Get(unit_task.address);
+      data_m = (data_m << 24) >> 24;
+      lsb->GetLoadedData(unit_task.value, data_m);
+      rs->GetLoadedData(unit_task.value, data_m);
+      rob->GetLoadedData(unit_task.value, data_m);
+    } else if (unit_task.type == InstructionType::LBU) {
+      const int32_t data_m = this->Get(unit_task.address);
+      lsb->GetLoadedData(unit_task.value, data_m);
+      rs->GetLoadedData(unit_task.value, data_m);
+      rob->GetLoadedData(unit_task.value, data_m);
+    } else if (unit_task.type == InstructionType::LH) {
+      int32_t data_m = (this->Get(unit_task.address) | (this->Get(unit_task.address + 1) << 8));
+      data_m = (data_m << 12) >> 12;
+      lsb->GetLoadedData(unit_task.value, data_m);
+      rs->GetLoadedData(unit_task.value, data_m);
+      rob->GetLoadedData(unit_task.value, data_m);
+    } else if (unit_task.type == InstructionType::LHU) {
+      const int32_t data_m = (this->Get(unit_task.address) | (this->Get(unit_task.address + 1) << 8));
+      lsb->GetLoadedData(unit_task.value, data_m);
+      rs->GetLoadedData(unit_task.value, data_m);
+      rob->GetLoadedData(unit_task.value, data_m);
+    } else if (unit_task.type == InstructionType::LW) {
+      const int32_t data_m = (this->Get(unit_task.address) | (this->Get(unit_task.address + 1) << 8)
+          | (this->Get(unit_task.address + 2) << 16) | (this->Get(unit_task.address + 3) << 24));
+      lsb->GetLoadedData(unit_task.value, data_m);
+      rs->GetLoadedData(unit_task.value, data_m);
+      rob->GetLoadedData(unit_task.value, data_m);
+    } else if (unit_task.type == InstructionType::SB) {
+      this->Add(unit_task.address, unit_task.value & 0xFF);
+    } else if (unit_task.type == InstructionType::SH) {
+      this->Add(unit_task.address, unit_task.value & 0xFF);
+      this->Add(unit_task.address + 1, (unit_task.value >> 8) & 0xFF);
+    } else if (unit_task.type == InstructionType::SW) {
+      this->Add(unit_task.address, unit_task.value & 0xFF);
+      this->Add(unit_task.address + 1, (unit_task.value >> 8) & 0xFF);
+      this->Add(unit_task.address + 2, (unit_task.value >> 16) & 0xFF);
+      this->Add(unit_task.address + 3, (unit_task.value >> 24) & 0xFF);
+    }
+  }
+}
+
+void Memory::SetTask(InstructionType type, int32_t address, int32_t value) {
+  task.memory_tasks[task.memory_tasks_len++] = {type, address, value};
 }
